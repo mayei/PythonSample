d93ae8cf70d21951c1067417925aba1fd1f238e5
mayei <einandrsoe2012@gail.com>
2020-01-13 15:30:57 +0630

    Tuple Sets

---
+++ List.py
@@ -0,0 +1,126 @@
+ # List
+
+ word = 'Programming'
+ word
+word[2]
+
+
+
+
+
+
+
+x = 'Hello World'
+
+
+
+
+P   r   o   g   r   a   m   m   i   n   g
+0   1   2   3   4   5   6   7   8   9   10
+-11-10 -9  -8   -7 -6  -5  -4  -3  -2   -1 
+
+
+
+
+ 
+
+
+
+
+
+
+ word[3:5]
+ word[5:9]
+ word[4:-4]
+ word[4:9]
+ word[:5]
+ word[8:]
+ word[3:]
+ word[:3]
+ word[:3 ]+ word[3:]
+ word[5:3]
+ word[:2] + word[3:]
+
+
+ len(word)
+ len(word) = len(x)
+
+ # len = Length
+
+ square = 'Square'
+ len(square)
+ len(word) + len(square)
+
+ cube = [10, 20, 30, 45, 50]
+ cube
+ cube[3] = 40
+ cube
+ cube[5] = 60
+ cube.append(4+10+20+36)
+ cube
+ cube.reverse()
+ cube
+ cube.remove(20)
+ cube.pop()
+ cube
+
+cube1 = []
+
+
+
+
+
+
+
+
+
+ 3 * 'A'
+ 2 * 'BC' + 
+
+ProgramA = []
+
+
+
+ProgramC = ['A', 'B', 'C',]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+https//docs.python.org/3/tutorial/datstructures.html
+5.1.3. List coprehensiions
+5.1.4. Nested list comprehensions
+5.2 The del Statement
+
+https://github.com/brainseeddeveloper/YTUpython
+
+>>> Tuple
