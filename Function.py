6edd0e77ab1a6ca84de4550b01a3cfc4f66e3089
mayei <einandrsoe2012@gail.com>
2020-02-11 14:56:15 +0630

    Function.py

--- Function.py
+++ Function.py
@@ -47,3 +47,129 @@ func(x)
 funx(x)
 x
 func(x) + funx(x)
+
+
+#Global Statement
+
+def func():
+	global x
+
+	print('x is',x)
+	x = 2
+	print('Changed global x to',x)
+
+x = 50
+func()
+print('Value of x is',x)
+
+x = 1
+y = 2
+global x = 10 y = 20
+
+#Default Arguement Values in Functions
+
+def say(,message, time=1):
+	print(message * times)
+
+say('Hello')
+say('World', 5)
+say('Good Bye')
+
+#keyword Argument
+
+def func(a, b=5, c=10):
+	print('a is',a, 'and b is',b, 'and c is', c)
+
+
+func(3, 8)
+func(24, c=26)
+func(c=29,a=39)
+
+#VarArgs prarmeters
+#function_VarArgs.py
+
+def total(a=5, *numbers, **phonebook):
+	print('a',a)
+
+	for single_item in numbers:
+		print('single_item',single_item)
+
+	for first_part,second_part in phonebook.items():
+		print(first_part,second_part)
+
+total(10, 1, 2, 3,Jack=1123,John=2231,Inge=1459)
+total(10, 1, 2, 3,12,15,33Jack=1123,John=2231,Inge=1459,Jame=2233)
+
+#Return statement
+
+def maximum(x, y):
+if x > y:
+	return x
+elif x == y:
+	retrn 'The numbers are equal'
+else:
+	return y
+
+print(maximum(3,8))
+print(maximum(20,10))
+
+#DocString (Documentation Strings)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def paper():
+	'''1. There will be situations where your 
+	program has to interact with the user.
+	function and print function respectively.
+
+	2. There will be situations where your 
+	program has to interact with the user.
+	function and print function respectively. '''
+
+
+	print(paper.__doc__)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
